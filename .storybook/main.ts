8229a7df66c7604ba3689ab505fb575cc17d4023
Sundeep <vallurisundeep@gmail.com>
2026-05-10 14:56:44 -0400

    cleanup

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,7 @@ import type { StorybookConfig } from '@storybook/angular';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: ['@storybook/addon-docs'],
+  addons: ['@storybook/addon-docs', '@storybook/addon-a11y'],
   staticDirs: ['../public'],
   framework: {
     name: '@storybook/angular',
